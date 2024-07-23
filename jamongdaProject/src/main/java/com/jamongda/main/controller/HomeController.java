ae34c3014e76d11c6cb7d50b6c0ccd7ba6b2955a
chanwoo0716 <chanwoo6195@naver.com>
2024-07-23 19:05:28 +0900

    2024/07/23 메인페이지 국내인기여행지, 최저가 숙소 카테고리 기능 구현

---
+++ jamongdaProject/src/main/java/com/jamongda/main/controller/HomeController.java
@@ -0,0 +1,64 @@
+package com.jamongda.main.controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jamongda.main.service.HomeControllerService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private HomeControllerService homeControllerService;
+	
+	@GetMapping("/main.do")
+	public ModelAndView main(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		//최저가 숙소 이미지 8개 가져오기
+		List<Map<String, Object>> lowestAccList = homeControllerService.lowestAcc();
+		
+        // lowestAccList 출력
+        System.out.println("lowestAccList: " + lowestAccList);
+		
+		// 이미지 파일 이름 리스트
+        List<String> lowestAccListImage = new ArrayList<>();
+		// acc_id 리스트
+        List<Integer> lowestAccListAcc_id = new ArrayList<>();
+        
+        // lowestAccList에서 acc_image와 acc_id 분리
+        for (Map<String, Object> map : lowestAccList) {
+            lowestAccListImage.add((String) map.get("acc_image"));
+            lowestAccListAcc_id.add((Integer) map.get("acc_id"));
+        }
+        
+        //System.out.println("lowestAccListImage:" + lowestAccListImage);
+        //System.out.println("lowestAccListAcc_id" + lowestAccListAcc_id);
+        
+		ModelAndView mav = new ModelAndView();
+		
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = today.plusDays(1);
+        
+        mav.addObject("today", today.toString());
+        mav.addObject("tomorrow", tomorrow.toString());
+        mav.addObject("datetimes", today.toString() + " - " + tomorrow.toString());
+		
+		
+		mav.addObject("lowestAccListImage",lowestAccListImage);
+		mav.addObject("lowestAccListAcc_id",lowestAccListAcc_id);
+		mav.setViewName("/main");
+		return mav;
+	}
+}
+
---
+++ jamongdaProject/src/main/java/com/jamongda/main/dao/HomeControllerDAO.java
@@ -0,0 +1,21 @@
+package com.jamongda.main.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("homeControllerDAO")
+public interface HomeControllerDAO {
+
+	//최저가 숙소 이미지 8개 가져오기
+	public List<Map<String, Object>> getLowestPricedAccImages() throws DataAccessException;
+	
+	//
+	
+	//
+}
