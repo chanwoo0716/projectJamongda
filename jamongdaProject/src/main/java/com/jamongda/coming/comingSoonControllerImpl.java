79fbd68ae63b6ab14db8867a2b9ac4dfab6ed6a2
wkddnjswl7 <wkddnjswl7@naver.com>
2024-07-23 10:20:14 +0900

    comingSoon Controller 오류난 로그와 이유 출력되게 동작

--- jamongdaProject/src/main/java/com/jamongda/coming/comingSoonControllerImpl.java
+++ jamongdaProject/src/main/java/com/jamongda/coming/comingSoonControllerImpl.java
@@ -7,24 +7,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ControllerAdvice
-public class comingSoonControllerImpl{
-	
-	//모든 예외를 처리하는 메서드
-	@ExceptionHandler(Exception.class)
+public class comingSoonControllerImpl {
+
+    private static final Logger logger = LoggerFactory.getLogger(comingSoonControllerImpl.class);
+
+    // 모든 예외를 처리하는 메서드
+    @ExceptionHandler(Exception.class)
     public ModelAndView handleError(HttpServletRequest request, Exception e) {
         ModelAndView mav = new ModelAndView();
-        
-        // 예외 객체를 모델에 추가
-        mav.addObject("exception", e);
-        
-        // 요청된 URL을 모델에 추가
-        mav.addObject("url", request.getRequestURL());
-        
+
+        // 예외 로그 기록
+        logger.error("Request: " + request.getRequestURL() + " raised " + e);
+
         // 에러페이지의 뷰 이름을 설정
         mav.setViewName("comingSoon");
-        
+
         return mav;
     }
 }
