a4718e4d647e688f986bf5293fb629ea87dd75cf
lynlim0904 <lynlim0904@gmail.com>
2024-07-25 01:41:36 +0900

    2024/07/25 숙소 상세페이지에 리뷰 구현(더보기, 이메일 마스킹), 예약시 숙박일자 계산해서 총결제금액 구현, 포맷팅

--- jamongdaProject/src/main/java/com/jamongda/review/dao/ReviewDAO.java
+++ jamongdaProject/src/main/java/com/jamongda/review/dao/ReviewDAO.java
@@ -1,5 +1,8 @@
 package com.jamongda.review.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
@@ -18,4 +21,10 @@ public interface ReviewDAO {
     public void insertReviewImage(ReviewImageDTO reviewImageDTO) throws DataAccessException;
     
     public void updateReviewImage(ReviewImageDTO reviewImageDTO) throws DataAccessException;
+    
+    // 숙소 ID로 작성한 리뷰와 이미지 리스트 가져오기
+    List<ReviewDTO> getReviewsByAccId(Map<String, Object> params) throws DataAccessException;
+
+    // ro_id를 통해 room의 ro_name 가져오기
+    String getRoomNameById(int ro_id) throws DataAccessException;
 }
