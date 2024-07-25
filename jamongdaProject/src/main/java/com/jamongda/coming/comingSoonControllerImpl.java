ef6e484943b1e113e05148986496d5bcaca0969d
lynlim0904 <lynlim0904@gmail.com>
2024-07-26 02:42:07 +0900

    2024/07/26 객실 이미지 팝업, 예약/마이페이지 스크립트, 숙소 위치 지도 구현

--- jamongdaProject/src/main/java/com/jamongda/coming/comingSoonControllerImpl.java
+++ jamongdaProject/src/main/java/com/jamongda/coming/comingSoonControllerImpl.java
@@ -16,16 +16,16 @@ public class comingSoonControllerImpl {
     private static final Logger logger = LoggerFactory.getLogger(comingSoonControllerImpl.class);
 
     // 모든 예외를 처리하는 메서드
-    @ExceptionHandler(Exception.class)
-    public ModelAndView handleError(HttpServletRequest request, Exception e) {
-        ModelAndView mav = new ModelAndView();
-
-        // 예외 로그 기록
-        logger.error("Request: " + request.getRequestURL() + " raised " + e);
-
-        // 에러페이지의 뷰 이름을 설정
-        mav.setViewName("comingSoon");
-
-        return mav;
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ModelAndView handleError(HttpServletRequest request, Exception e) {
+//        ModelAndView mav = new ModelAndView();
+//
+//        // 예외 로그 기록
+//        logger.error("Request: " + request.getRequestURL() + " raised " + e);
+//
+//        // 에러페이지의 뷰 이름을 설정
+//        mav.setViewName("comingSoon");
+//
+//        return mav;
+//    }
 }
