8b9fd2261bf5ab7cd1a717ef2950243ea86e17b9
lynlim0904 <lynlim0904@gmail.com>
2024-08-08 17:42:04 +0900

    2024/08/08 코드 정리 및 main 숙소 이름 출력

--- jamongdaProject/src/main/java/com/jamongda/search/dao/SearchDAO.java
+++ jamongdaProject/src/main/java/com/jamongda/search/dao/SearchDAO.java
@@ -15,31 +15,36 @@ import com.jamongda.search.dto.SearchDTO;
 
 @Mapper
 public interface SearchDAO {
-   public List selectAllAccsList() throws DataAccessException;
-   //2024-07-20수정
-   public List searchAccList(AccommodationDTO acc) throws DataAccessException;
-   
-   public List searchAccType(SearchDTO acc) throws DataAccessException;
-   //2024-07-20수정완료
-   
-   public AccommodationDTO selectAccId(int acc_id) throws DataAccessException;
-   
-   public AccommodationDTO selectRadio(AccommodationDTO acc) throws DataAccessException;
-   
-   public List searchImage(AccommodationDTO acc) throws DataAccessException;
-   
-   public List<SearchDTO> searchPriceRange(@Param("minPrice") int minPrice, @Param("maxPrice") int maxPrice) throws DataAccessException;
-   
-    // acc_id에 해당하는 숙소 가져오기
-    public List<AccommodationDTO> selectAcc(int acc_id) throws DataAccessException;
-    // acc_id에 해당하는  객실 정보 가져오기
-    public List<RoomDTO> selectRo(int acc_id) throws DataAccessException;
-    // acc_id에 해당하는  숙소 이미지 가져오기
-    public List<AccommodationImageDTO> selectAccImages(int acc_id) throws DataAccessException;
-    // acc_id에 해당하는  객실 이미지 가져오기
-    public List<RoomImageDTO> selectRoImages(int acc_id) throws DataAccessException;
-    
-   // 대표자명, 사업자번호 추가
-      public Map<String, Object> selectHostInfo(int acc_id) throws Exception;
-    
+	public List selectAllAccsList() throws DataAccessException;
+
+	public List searchAccList(AccommodationDTO acc) throws DataAccessException;
+
+	public List searchAccType(SearchDTO acc) throws DataAccessException;
+
+	public AccommodationDTO selectAccId(int acc_id) throws DataAccessException;
+
+	public AccommodationDTO selectRadio(AccommodationDTO acc) throws DataAccessException;
+
+	public List<SearchDTO> searchPriceRange(@Param("minPrice") int minPrice, @Param("maxPrice") int maxPrice)
+			throws DataAccessException;
+
+	// acc_id에 해당하는 숙소 가져오기
+	public List<AccommodationDTO> selectAcc(int acc_id) throws DataAccessException;
+
+	// acc_id에 해당하는 객실 정보 가져오기
+	public List<RoomDTO> selectRo(int acc_id) throws DataAccessException;
+
+	// acc_id에 해당하는 숙소 이미지 가져오기
+	public List<AccommodationImageDTO> selectAccImages(int acc_id) throws DataAccessException;
+
+	// acc_id에 해당하는 객실 이미지 가져오기
+	public List<RoomImageDTO> selectRoImages(int acc_id) throws DataAccessException;
+
+	// 대표자명, 사업자번호 추가
+	public Map<String, Object> selectHostInfo(int acc_id) throws Exception;
+
+	// 가격, 숙소유형 동시검색
+	public List<SearchDTO> searchByTypeAndPrice(@Param("acc_type") String acc_type, @Param("minPrice") int minPrice,
+			@Param("maxPrice") int maxPrice) throws DataAccessException;
+
 }
